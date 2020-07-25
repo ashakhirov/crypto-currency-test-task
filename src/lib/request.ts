280536f85269e2144cf0be358dfc8abccb311fc3
shahirov <ara.shahirov@gmail.com>
2020-07-25 20:09:41 +0500

    add helper function for http request

---
+++ src/lib/request.ts
@@ -0,0 +1,36 @@
+type Method = 'get'
+type RequestAttr = {
+  method: Method
+  path: string
+}
+type GetAttr = Omit<RequestAttr, 'method'>
+type GetRequest = <T>({ path }: GetAttr) => Promise<T>
+type Request = <T>({ method, path }: RequestAttr) => Promise<T>
+
+/**
+ * make request
+ * @param {string} method request method
+ * @param {string} path request url
+ * @returns {Promise<T>} response
+ */
+const request: Request = async ({ method, path }) => {
+  const options: RequestInit = {
+    method: method.toUpperCase(),
+    headers: new Headers({
+      authorization: `Apikey ${process.env.API_KEY}`,
+    }),
+  }
+  const url = new URL(path, process.env.API_URL)
+  console.log(url.href)
+
+  const response = await fetch(url.href, options)
+
+  return response.json()
+}
+
+/**
+ * make get request
+ * @param {string} path request url
+ * @returns {Promise<T>} response
+ */
+export const get: GetRequest = ({ path }) => request({ method: 'get', path })
